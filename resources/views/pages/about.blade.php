add4329f38dd19f457e1c4e020adf55713118620
tomoya <t0819t@ezweb.ne.jp>
2020-08-05 04:47:20 +0000

    tutorial

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>{{config("app.name","PKProject")}}</title>
+    </head>
+    <body>
+        <h1><b>これはお試しです</b></h1><br>
+        
+       
+        
+    </body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width-device-width,initial-scale-1">
+        
+        <title>{{config('app.name','PKProject')}}</title>
+    </head>
+    <body>
+        <h1><b>ようこそ！プログラミングの世界へ</b></h1>
+        <p>これは初心者むけプログラムです</p>
+       
+        
+    </body>
+</html>
